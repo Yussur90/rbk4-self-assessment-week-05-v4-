4185966ffc35b2b89c173b20ead8baa666a90d71
yussur90 <yuyualani@yahoo.com>
2018-04-14 09:58:15 +0300

    final

--- node-express-sequelize/server.js
+++ node-express-sequelize/server.js
@@ -7,6 +7,27 @@ var User = sequelize.define('User', {
   username: Sequelize.STRING
 });
 
+
+app.get('/users',function(req,res,cb){
+User.find(User,function(err,username){
+if(err,null){
+
+	console.log(err)
+	cb()
+}
+
+else if(null,username){
+
+res.send(req.body.username)
+cb()
+}
+
+})
+})
+
+
+
+
 /*  Create a '/users' route that responds to 
     a GET request with all users in the database */
 
